a3f5892493f1752e8b8ddea50d84453471c4f740
FoxWithAShotgun <31714271+FoxWithAShotgun@users.noreply.github.com>
2020-03-08 17:02:32 +0800

    Fixing up the Soul Reaper property of Revia race. Should no longer cause problems if for some reason the Revia ends up having its tail count hediff removed by another mod.
    Fixing hair tags and About.xml. Also unsetting the auto-flee of Revian marauders.

--- Source/Comps/InvokeBlessing.cs
+++ Source/Comps/InvokeBlessing.cs
@@ -6,53 +6,23 @@ using System.Text;
 using System.Threading.Tasks;
 using Verse;
 
-namespace ReviaRace
+namespace ReviaRace.Comps
 {
     public class InvokeBlessing : CompUseEffect
     {
-        public override float OrderPriority => 0;
-
-        private int GetSoulReapTier(Pawn pawn)
-        {
-            if (pawn == null)
-            {
-                return -1;
-            }
-            
-            var hediff = pawn.health.hediffSet.hediffs.First(h => h.Label.Contains("Soul Reap"));
-            if (hediff == null)
-            {
-                return -1;
-            }
-
-            var tier = -1;
-            switch (hediff.def.label)
-            {
-                case "Soul Reap (1)": tier = 1; break;
-                case "Soul Reap (2)": tier = 2; break;
-                case "Soul Reap (3)": tier = 3; break;
-                case "Soul Reap (4)": tier = 4; break;
-                case "Soul Reap (5)": tier = 5; break;
-                case "Soul Reap (6)": tier = 6; break;
-                case "Soul Reap (7)": tier = 7; break;
-                case "Soul Reap (8)": tier = 8; break;
-                case "Soul Reap (9)": tier = 9; break;
-                default: break;
-            }
-
-            return tier;
-        }
-
         public override void DoEffect(Pawn pawn)
         {
             base.DoEffect(pawn);
+            var srComp = pawn.GetComp<SoulReaper>();
+            Messages.Message($"{pawn.Name} offers some bloodstones to the bloody god.", pawn, MessageTypeDefOf.NeutralEvent, false);
 
-            if (pawn.MapHeld == null)
+            if (pawn.MapHeld == null || srComp == null)
             {
+                Messages.Message($"Invalid target. srcomp null = {srComp == null}", pawn, MessageTypeDefOf.CautionInput, false);
                 return;
             }
 
-            var soulReapTier = GetSoulReapTier(pawn);
+            var soulReapTier = srComp.GetSoulReapTier();
             if (soulReapTier == -1)
             {
                 Messages.Message("The bloodstone does nothing for the uninitiated...", pawn, MessageTypeDefOf.NegativeEvent, false);
@@ -64,64 +34,47 @@ namespace ReviaRace
                 return;
             }
 
-            // Roll the dice!
-            if (CalculateAdvanceSuccess(pawn))
+            // Check that we have enough bloodstones in the stack to proceed.
+            int cost = CalculateAdvanceCost(soulReapTier);
+            if (cost > parent.stackCount)
             {
-                AdvanceSoulreapTier(pawn);
+                Messages.Message($"{pawn.Name}'s offering was rejected. The blood god demands more blood.", pawn, MessageTypeDefOf.NeutralEvent, false);
+                return;
             }
 
-            // Destroy one bloodstone on use, regardless of success.
-            parent.stackCount--;
+            // Blood for the blood god! Skulls for the skull throne!
+            srComp.RemoveSoulReapHediffs();
+            srComp.AddSoulReapTier(soulReapTier + 1);
+            Messages.Message($"The blessing has succeeded, and {pawn.Name} has grown stronger.", pawn, MessageTypeDefOf.PositiveEvent);
+
+            // Destroy the bloodstones that were used up. If there were exactly enough, destroy the stack.
+            if (parent.stackCount == cost)
+            {
+                parent.Destroy();
+            }
+            else
+            {
+                parent.stackCount -= cost;
+            }
         }
 
         /// <summary>
-        /// Pray to RNGesus for success.
-        /// Probability of success is 1 / (2^(n-2)),
-        /// guaranteed when n < 3.
-        /// Soul Reap tier 9 cannot ever advance, as it is the max level.
+        /// The cost of advancing is 2 ^ (n-2) bloodstones, 
         /// </summary>
         /// <param name="pawn">The pawn to check</param>
         /// <returns>True if the random number generated passes the RNG check.
         /// False if the hediff wasn't a soulreap that can be levelled up, 
         /// or if the RNG check failed.</returns>
-        private bool CalculateAdvanceSuccess(Pawn pawn)
+        private int CalculateAdvanceCost(int tier)
         {
-            var tier = GetSoulReapTier(pawn);
             if (tier == -1 || tier == 9)
             {
-                return false;
+                return -1;
             }
 
-            Random rng = new Random();
+            double cost = Math.Max(1.0, Math.Pow(2, tier - 2));
 
-            double roll = rng.NextDouble();
-            double threshold = 1 / (Math.Pow(2, tier - 2));
-
-            return roll <= threshold;
-        }
-
-        /// <summary>
-        /// Advances the soul reap tier of a pawn. No effect on Tier 9 soul reaps,
-        /// or if the pawn doesn't have it to begin with.
-        /// </summary>
-        /// <param name="pawn">The pawn to advance.</param>
-        private void AdvanceSoulreapTier(Pawn pawn)
-        {
-            var soulReapTier = GetSoulReapTier(pawn);
-            if (soulReapTier == -1 || soulReapTier == 9)
-            {
-                return;
-            }
-
-            var toAdd = HediffDef.Named($"ReviaRaceSoulreapTier{soulReapTier + 1}");
-            var toRemove = pawn.health.hediffSet.GetFirstHediffOfDef(HediffDef.Named($"ReviaRaceSoulreapTier{soulReapTier}"));
-            
-            if (toAdd != null)
-            {
-                pawn.health.AddHediff(toAdd);
-                pawn.health.RemoveHediff(toRemove);
-                Messages.Message($"The blessing has succeeded, and {pawn.Name} has grown stronger.", pawn, MessageTypeDefOf.PositiveEvent);
-            }
+            return (int)cost;
         }
     }
 }
---
+++ Source/Comps/SoulReaper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace ReviaRace.Comps
+{
+    public class SoulReaper : ThingComp
+    {
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+            var pawn = parent as Pawn;
+            
+            if (pawn != null && GetSoulReapTier() == -1)
+            {
+                // This should have at least tier 1 of soul reap.
+                // Add it now.
+                var rng = new Random();
+                var tier = rng.Next(1, 3);
+                AddSoulReapTier(tier);
+            }
+        }
+
+        private Hediff SoulReapHediff => (parent as Pawn)?.health.hediffSet.hediffs
+                                         .FirstOrDefault(hediff => hediff.def.label.Contains("Soul Reap"));
+
+        internal int GetSoulReapTier()
+        {
+            var tier = -1;
+
+            if (SoulReapHediff != null)
+            {
+                switch (SoulReapHediff.def.label)
+                {
+                    case "Soul Reap (1)": tier = 1; break;
+                    case "Soul Reap (2)": tier = 2; break;
+                    case "Soul Reap (3)": tier = 3; break;
+                    case "Soul Reap (4)": tier = 4; break;
+                    case "Soul Reap (5)": tier = 5; break;
+                    case "Soul Reap (6)": tier = 6; break;
+                    case "Soul Reap (7)": tier = 7; break;
+                    case "Soul Reap (8)": tier = 8; break;
+                    case "Soul Reap (9)": tier = 9; break;
+                    default: break;
+                }
+            }
+
+            return tier;
+        }
+
+        internal void AddSoulReapTier(int tier)
+        {
+            var pawn = parent as Pawn;
+            if (tier < 0 || tier > 9 || 
+                pawn == null)
+            {
+                return;
+            }
+
+            HediffDef toAdd = HediffDef.Named($"ReviaRaceSoulreapTier{tier}");
+            pawn.health.AddHediff(toAdd);
+        }
+
+        internal void RemoveSoulReapHediffs()
+        {
+            var pawn = parent as Pawn;
+            if (pawn == null)
+            {
+                return;
+            }
+
+            var hediffsToRemove = pawn.health.hediffSet.hediffs.Where(hediff => hediff.def.label.Contains("Soul Reap")).ToList();
+            foreach (var hediff in hediffsToRemove)
+            {
+                pawn.health.RemoveHediff(hediff);
+            }
+        }
+    }
+}
