3cb67703e0e428bc47c3b1c4c7d6729a8278d46e
FoxWithAShotgun <31714271+FoxWithAShotgun@users.noreply.github.com>
2020-03-25 06:37:49 +0800

    Various fixes. Added Blood Runes and sanctified equipment.

---
+++ Source/RoomRoleWorkers/SkarneChapel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace ReviaRace.RoomRoleWorkers
+{
+    public class SkarneChapel : RoomRoleWorker
+    {
+        public override float GetScore(Room room)
+        {
+            float score = room.ContainsThing(ThingDef.Named("ReviaBloodSigil")) ? 1.0f : 0.0f;
+            return score;
+        }
+    }
+}
