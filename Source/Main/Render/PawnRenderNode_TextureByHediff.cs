024a456f4dcb6420991ad3a516e4375b165c71db
MaxDorob <maxdor81@gmail.com>
2024-04-03 03:10:56 +0600

    Made my own texture path resolver

---
+++ Source/Main/Render/PawnRenderNode_TextureByHediff.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace ReviaRace
+{
+    public class PawnRenderNode_TextureByHediff : PawnRenderNode
+    {
+        public PawnRenderNode_TextureByHediff(Pawn pawn, PawnRenderNodeProperties props, PawnRenderTree tree) : base(pawn, props, tree)
+        {
+        }
+        PawnRendererNodeProperties_TextureByHediff Props => props as PawnRendererNodeProperties_TextureByHediff;
+        protected override string TexPathFor(Pawn pawn)
+        {
+            foreach (var item in Props.hediffTex)
+            {
+                if (pawn.health.hediffSet.HasHediff(item.hediff)) return item.path;
+            }
+            Log.ErrorOnce($"Unable to find texture for {pawn}", 2902395);
+            return base.TexPathFor(pawn);
+        }
+    }
+}
---
+++ Source/Main/Render/PawnRendererNodeProperties_TextureByHediff.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace ReviaRace
+{
+    public class PawnRendererNodeProperties_TextureByHediff : PawnRenderNodeProperties
+    {
+        public List<HediffTexture> hediffTex = [];
+    }
+}
