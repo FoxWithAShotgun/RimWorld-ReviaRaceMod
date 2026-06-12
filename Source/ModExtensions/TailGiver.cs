3cc541577b5972ae93d32082ca75a4bbda392922
Lord Navry <gutelius.b@gmail.com>
2026-06-12 02:53:18 -0800

    Re-asserting Tail Giver commit.

--- Source/ModExtensions/TailGiver.cs
+++ Source/ModExtensions/TailGiver.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RimWorld;
 using Verse;
 
 namespace ReviaRace.ModExtensions
 {
-    public class SoulreapTailGiver_Extension : DefModExtension
-    {
+	public class SoulreapTailGiver_Extension : DefModExtension
+	{
         public IntRange tier = new IntRange(1, 9);
     }
 }
